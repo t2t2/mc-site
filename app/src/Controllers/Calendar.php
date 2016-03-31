787483865fd136a5797b82b52db21e14cebce86f
t2t2 <taavot@gmail.com>
2016-04-01 01:27:32 +0300

    Some calendar code I already had written

---
+++ app/src/Controllers/Calendar.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Mindcrack\Site\Controllers;
+
+
+use Bolt\Controllers\Frontend;
+use Bolt\Storage;
+use Silex\Application;
+
+class Calendar extends Frontend {
+
+	/**
+	 * Get the upcoming events
+	 * 
+	 * @param Application $app
+	 * @param             $contentTypeSlug
+	 * @param string      $template
+	 *
+	 * @return \Twig_Markup
+	 */
+	public function upcoming(Application $app, $contentTypeSlug, $template = 'listing.twig') {
+		/** @var Storage $storage */
+		$storage = $app['storage'];
+		$contentType = $storage->getContentType($contentTypeSlug);
+		
+		$content = $storage->getContent($contentType['slug'], [
+			'event_time' => '> 3 days ago',
+			'limit' => 50,
+			'order' => 'event_time'
+		]);
+		
+		// Make sure we can also access it as {{ pages }} for pages, etc. We set these in the global scope,
+		// So that they're also available in menu's and templates rendered by extensions.
+		$app['twig']->addGlobal('records', $content);
+		$app['twig']->addGlobal($contentType['slug'], $content);
+		$app['twig']->addGlobal('contenttype', $contentType['name']);
+
+		// Group the data into months
+		$groupedByMonth = [];
+		
+		
+		
+		return $this->render($app, $template, $contentType);
+	}
+}
