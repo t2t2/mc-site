ad5e1fca4f17d8471f400d0dc98cc7ff389b35f8
t2t2 <taavot@gmail.com>
2016-05-13 14:21:22 +0300

    Removing gulp files as they're no longer needed

--- gulpfile.js/tasks/default.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-
-module.exports = function (cb) {
-	var gulpSequence = require('gulp-sequence')
-
-	gulpSequence('clean', ['assets'], ['css'], ['js'], 'watch', cb)
-}
-gulp.task('default', module.exports);
